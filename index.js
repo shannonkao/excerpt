10300a1fbfdbf3c926b71acc83a8827a02edf9f1
shannon <skao127@gmail.com>
2019-07-06 18:10:52 -0700

    Brontes + Keats

--- index.js
+++ index.js
@@ -21,36 +21,41 @@ xhr.onload = (e) => {
     name_div.innerHTML = name.join(' ');
 
     let nickname_div = document.getElementById("nickname");
-    nickname_div.innerHTML = "⸢" + nickname(name).replace(punct_regex, '') + "⸣";
+    nickname_div.innerHTML = "⸢" + nickname(name) + "⸣";
 }
 
 function populateUrls() {
     let urls = [];
     urls.push('http://poetrydb.org/author,linecount/Shakespeare;14/lines');
     urls.push('http://poetrydb.org/author/Dickinson/lines');
-    urls.push('http://poetrydb.org/author/Tennyson/lines')
+    urls.push('http://poetrydb.org/author/Tennyson/lines');
+    urls.push('http://poetrydb.org/author/Keats/lines');
+    urls.push('http://poetrydb.org/author/Bronte/lines');
     
     return urls;
 }
 
 function getLine(poem) {
     let lines = poem.lines.filter( (e) => !!e );
-    return lines[rand(0, lines.length)];
+    let line = lines[rand(0, lines.length)].replace(/"|\r\n|\n|\r/gm, '');
+    return (line[line.length - 1] == "'") ? line.slice(0, line.length - 1) : line;
 }
 
 function nickname(arr) {
     let filtered = arr.filter( (e) => {
-        e = e.replace(punct_regex, '');
-        return (e && !ignore.includes(e.toLowerCase()))
+        e = e.replace(punct_regex, '').trim();
+        return (!!e && !ignore.includes(e.toLowerCase()))
     });
 
     let l = filtered.length - 1;
-    return l >= 0 ? filtered[rand(0, l)] : arr[rand(0, arr.length - 1)];
+    let nickname = l >= 0 ? filtered[rand(0, l)] : arr[rand(0, arr.length - 1)];
+    return (punct_regex.test(nickname[nickname.length - 1])) ? nickname.slice(0, nickname.length - 1) : nickname;
 }
 
 function modernize(str) {
     return str.replace(/\bthee\b/gi, 'you')
               .replace(/\bthou\b/gi, 'you')
+              .replace(/\bye\b/gi, 'you')
               .replace(/\bthy\b/gi, 'your')
               .replace(/\bthine\b/gi, 'your')
               .replace(/\bthyself\b/gi, 'yourself')
@@ -73,4 +78,4 @@ function rand(start, end) {
     return Math.floor(Math.random() * (end - start) + start);
 }
 
-const ignore = ["i", "a", "aboard", "about", "above", "across", "afore", "after", "against", "all", "along", "alongside", "am", "amid", "amidst", "among", "amongst", "an", "and", "anenst", "another", "any", "anybody", "anyone", "anything", "apud", "are", "around", "as", "aside", "astride", "at", "athwart", "atop", "barring", "been", "before", "behind", "being", "below", "beneath", "beside", "besides", "between", "beyond", "both", "but", "by", "can", "circa", "could", "dare", "despite", "did", "do", "does", "down", "during", "each", "either", "enough", "everybody", "everyone", "everything", "except", "excluding", "failing", "few", "following", "for", "forenenst", "from", "given", "had", "has", "have", "having", "he", "her", "hers", "herself", "him", "himself", "his", "in", "including", "inside", "into", "is", "it", "itself", "lest", "like", "little", "many", "may", "me", "mid", "midst", "might", "mine", "minus", "modulo", "more", "most", "much", "must", "myself", "near", "need", "neither", "next", "no one", "nobody", "none", "nor", "nothing", "notwithstanding", "of", "off", "on", "one", "onto", "opposite", "or", "other", "others", "ought", "ours", "ourselves", "out", "outside", "over", "pace", "past", "per", "plus", "pro", "qua", "regarding", "round", "sans", "save", "several", "shall", "she", "should", "since", "so", "some", "somebody", "someone", "something", "such", "than", "that", "theirs", "them", "themselves", "these", "they", "this", "those", "through", "throughout", "till", "times", "to", "toward", "towards", "under", "underneath", "unlike", "until", "unto", "up", "upon", "us", "versus", "very", "via", "vice", "was", "we", "well", "were", "what", "whatever", "which", "whichever", "who", "whoever", "whom", "there", "whomever", "whose", "will", "with", "within", "without", "worth", "would", "yet", "you", "yours", "yourself", "be", "form", "far", "saw", "little", "that's", "where", "only", "my", "pass", "though", "self", "your", "here", "the", "if", "then", "what", "why", "how", "no", "come", "now", "not", "look", "o", "back", "their", "give", "because", "never", "better", "still", "too", "took", "else", "gave", "when", "seen", "cannot"]
+const ignore = ["i", "a", "aboard", "about", "above", "across", "afore", "after", "against", "all", "along", "alongside", "am", "amid", "amidst", "among", "amongst", "an", "and", "anenst", "another", "any", "anybody", "anyone", "anything", "apud", "are", "around", "as", "aside", "astride", "at", "athwart", "atop", "barring", "been", "before", "behind", "being", "below", "beneath", "beside", "besides", "between", "beyond", "both", "but", "by", "can", "circa", "could", "dare", "despite", "did", "do", "does", "down", "during", "each", "either", "enough", "everybody", "everyone", "everything", "except", "excluding", "failing", "few", "following", "for", "forenenst", "from", "given", "had", "has", "have", "having", "he", "her", "hers", "herself", "him", "himself", "his", "in", "including", "inside", "into", "is", "it", "itself", "lest", "like", "little", "many", "may", "me", "mid", "midst", "might", "mine", "minus", "modulo", "more", "most", "much", "must", "myself", "near", "need", "neither", "next", "no one", "nobody", "none", "nor", "nothing", "notwithstanding", "of", "off", "on", "one", "onto", "opposite", "or", "other", "others", "ought", "ours", "ourselves", "out", "outside", "over", "pace", "past", "per", "plus", "pro", "qua", "regarding", "round", "sans", "save", "several", "shall", "she", "should", "since", "so", "some", "somebody", "someone", "something", "such", "than", "that", "theirs", "them", "themselves", "these", "they", "this", "those", "through", "throughout", "till", "times", "to", "toward", "towards", "under", "underneath", "unlike", "until", "unto", "up", "upon", "us", "versus", "very", "via", "vice", "was", "we", "well", "were", "what", "whatever", "which", "whichever", "who", "whoever", "whom", "there", "whomever", "whose", "will", "with", "within", "without", "worth", "would", "yet", "you", "yours", "yourself", "be", "form", "far", "saw", "little", "that's", "where", "only", "my", "pass", "though", "self", "your", "here", "the", "if", "then", "what", "why", "how", "no", "come", "now", "not", "look", "o", "back", "their", "give", "because", "never", "better", "still", "too", "took", "else", "gave", "when", "seen", "cannot", "its", "ah", "while", "went"]
